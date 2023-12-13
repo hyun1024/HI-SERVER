48c4f11dc488eb1105812304c924bb97b16abe47
hyun1024 <haema3043@gmail.com>
2023-12-13 23:30:45 +0900

    initial commit

---
+++ src/main/java/com/teamh/hiserver/HiServerApplication.java
@@ -0,0 +1,13 @@
+package com.teamh.hiserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HiServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HiServerApplication.class, args);
+    }
+
+}
