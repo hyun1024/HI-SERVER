ad683c9b5370b2c56169231373dc785d4251de13
hyun1024 <haema3043@gmail.com>
2023-12-13 23:46:43 +0900

    Feat: view Controller

---
+++ src/main/java/com/teamh/hiserver/view/ViewController.java
@@ -0,0 +1,13 @@
+package com.teamh.hiserver.view;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class ViewController {
+
+    @RequestMapping("/auction")
+    public String RendAuction(){
+        return "auction";
+    }
+}
