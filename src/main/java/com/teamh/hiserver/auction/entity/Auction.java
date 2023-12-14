a361111f19f143eaf93efaeea8b4f92a8671a939
hyun1024 <haema3043@gmail.com>
2023-12-15 02:10:05 +0900

    Feat: create Auction Class

---
+++ src/main/java/com/teamh/hiserver/auction/entity/Auction.java
@@ -0,0 +1,36 @@
+package com.teamh.hiserver.auction.entity;
+
+import com.teamh.hiserver.Item.entity.Item;
+import com.teamh.hiserver.user.entity.User;
+import jakarta.persistence.*;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Entity
+@NoArgsConstructor
+public class Auction {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long auctionId;
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User sellUser;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User buyUser;
+
+    @ManyToOne
+    @JoinColumn(name="item_id")
+    private Item item;
+
+    @Column
+    private Long currentPrice;
+    @Column
+    private Long quickSellPrice;
+    @Column
+    private LocalDateTime registrationTime;
+}
