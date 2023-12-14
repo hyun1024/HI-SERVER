318f173b7cfbde682f897d6eb96c5f9c385881d7
hyun1024 <haema3043@gmail.com>
2023-12-14 23:56:13 +0900

    Refactor: modified Item Class

--- src/main/java/com/teamh/hiserver/Item/entity/Item.java
+++ src/main/java/com/teamh/hiserver/Item/entity/Item.java
@@ -13,46 +13,23 @@ public class Item {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long itemId;
     @Column(nullable = false)
+    private String itemName;
+    @Column(nullable = false)
     private String imageUrl;
     @Column
     private Long itemType;
     @Column
-    private Integer attack;
-    @Column
-    private Integer armor;
-    @Column
-    private Integer hp;
-    @Column
-    private Integer mp;
-    @Column
     private Long price;
     @Column
-    private Integer level;
-    @Column
-    private Integer strength;
-    @Column
-    private Integer dex;
-    @Column
-    private Integer intellect;
-    @Column
-    private Integer luck;
-    @Column
-    private Integer upgrade;
+    private Integer usableLv;
+
     @Builder
-    public Item(Long itemId, String imageUrl, Long itemType, Integer attack, Integer armor, Integer hp, Integer mp, Long price, Integer level, Integer strength, Integer dex, Integer intellect, Integer luck, Integer upgrade) {
+    public Item(Long itemId, String itemName, String imageUrl, Long itemType, Long price, Integer level) {
         this.itemId = itemId;
+        this.itemName = itemName;
         this.imageUrl = imageUrl;
         this.itemType = itemType;
-        this.attack = attack;
-        this.armor = armor;
-        this.hp = hp;
-        this.mp = mp;
         this.price = price;
-        this.level = level;
-        this.strength = strength;
-        this.dex = dex;
-        this.intellect = intellect;
-        this.luck = luck;
-        this.upgrade = upgrade;
+        this.usableLv = level;
     }
 }
