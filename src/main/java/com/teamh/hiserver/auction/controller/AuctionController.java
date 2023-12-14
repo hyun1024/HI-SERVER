677d0557947cb4f5d901604249372b533b754c3c
hyun1024 <haema3043@gmail.com>
2023-12-14 23:10:45 +0900

    Feat: registration method

--- src/main/java/com/teamh/hiserver/auction/controller/AuctionController.java
+++ src/main/java/com/teamh/hiserver/auction/controller/AuctionController.java
@@ -1,11 +1,20 @@
 package com.teamh.hiserver.auction.controller;
 
+import com.teamh.hiserver.Item.entity.Item;
 import com.teamh.hiserver.auction.service.AuctionService;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
 public class AuctionController {
     private final AuctionService auctionService;
+
+    @RequestMapping("/auction/registration")
+    public void itemRegistration(@RequestBody Item item, HttpServletRequest request){
+        auctionService.itemRegistration(item, request);
+    }
 }
--- src/main/java/com/teamh/hiserver/auction/service/AuctionService.java
+++ src/main/java/com/teamh/hiserver/auction/service/AuctionService.java
@@ -1,6 +1,8 @@
 package com.teamh.hiserver.auction.service;
 
+import com.teamh.hiserver.Item.entity.Item;
 import com.teamh.hiserver.auction.repository.AuctionRepository;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -9,4 +11,9 @@ import org.springframework.stereotype.Service;
 public class AuctionService {
 
     private final AuctionRepository auctionRepository;
+
+    public void itemRegistration(Item item, HttpServletRequest request) {
+
+
+    }
 }
