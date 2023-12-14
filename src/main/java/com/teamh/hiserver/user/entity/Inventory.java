baf7b974a6cb5feb1f0c240388273bb5e40daa61
hyun1024 <haema3043@gmail.com>
2023-12-15 02:04:58 +0900

    Feat: set Inventory Class

--- src/main/java/com/teamh/hiserver/user/entity/Inventory.java
+++ src/main/java/com/teamh/hiserver/user/entity/Inventory.java
@@ -1,5 +1,6 @@
 package com.teamh.hiserver.user.entity;
 
+import com.teamh.hiserver.Item.entity.Item;
 import jakarta.persistence.*;
 import lombok.NoArgsConstructor;
 
@@ -10,4 +11,21 @@ public class Inventory {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name = "item_id")
+    private Item item;
+
+    @Column
+    private Integer slot;
+
+    public Inventory(Long id, User user, Item item, Integer slot) {
+        this.id = id;
+        this.user = user;
+        this.item = item;
+        this.slot = slot;
+    }
 }
