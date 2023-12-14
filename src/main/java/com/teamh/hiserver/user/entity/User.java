9cf82db3f6f1d6e57cff6fef6a203358b963eb96
hyun1024 <haema3043@gmail.com>
2023-12-14 23:58:01 +0900

    Refactor: modified User Class

--- src/main/java/com/teamh/hiserver/user/entity/User.java
+++ src/main/java/com/teamh/hiserver/user/entity/User.java
@@ -1,16 +1,38 @@
 package com.teamh.hiserver.user.entity;
 
 import jakarta.persistence.*;
-import lombok.Getter;
+import lombok.Builder;
 import lombok.NoArgsConstructor;
 @Entity
 @NoArgsConstructor
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    private Long userId;
 
     @Column
     private String loginId;
-
+    @Column
+    private String nickname;
+    @Column
+    private String password;
+    @Column
+    private Integer level;
+    @Column
+    private Long exp;
+    @Column
+    private Long gold;
+    @Column
+    private Long medal;
+    @Builder
+    public User(Long userId, String loginId, String nickname, String password, Integer level, Long exp, Long gold, Long medal) {
+        this.userId = userId;
+        this.loginId = loginId;
+        this.nickname = nickname;
+        this.password = password;
+        this.level = level;
+        this.exp = exp;
+        this.gold = gold;
+        this.medal = medal;
+    }
 }
