311e050dfa8369eeb6fd36b6b78cc3664b21d04a
hyun1024 <haema3043@gmail.com>
2023-12-16 22:45:21 +0900

    Feat: set method for REST API

--- src/main/java/com/teamh/hiserver/user/controller/UserController.java
+++ src/main/java/com/teamh/hiserver/user/controller/UserController.java
@@ -8,23 +8,22 @@ import com.teamh.hiserver.user.service.UserService;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/api/users")
 public class UserController {
 
     private final UserService userService;
-    @RequestMapping("/api/user/signup")
+    @PostMapping("/signup")
     public ResponseEntity<SignupResponseDto> signup(@RequestBody SignupRequestDto signupRequestDto, HttpServletRequest request){
         SignupResponseDto signupResponseDto = userService.signup(signupRequestDto, request);
 
         return ResponseEntity.status(201).body(signupResponseDto);
     }
 
-    @RequestMapping("/api/user/login")
+    @PostMapping("/login")
     public ResponseEntity<LoginResponseDto> login(@RequestBody LoginRequestDto loginRequestDto, HttpServletRequest request){
         LoginResponseDto loginResponseDto = userService.login(loginRequestDto, request);
 
