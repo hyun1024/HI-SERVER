52e9b89ee23ba9e9553fe95951e1f5af6b1c299f
hyun1024 <haema3043@gmail.com>
2023-12-17 04:57:07 +0900

    Fix: add path for view

--- src/main/java/com/teamh/hiserver/global/view/ViewController.java
+++ src/main/java/com/teamh/hiserver/global/view/ViewController.java
@@ -1,18 +1,19 @@
-package com.teamh.hiserver.view;
+package com.teamh.hiserver.global.view;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/view")
 public class ViewController {
 
     @RequestMapping("/auction")
-    public String RendAuction(){
+    public String rendAuction(){
         return "auction";
     }
 
     @RequestMapping("/signup")
-    public String RendSignup(){
+    public String rendSignup(){
         return "signup";
     }
 }
